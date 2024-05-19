e2120083192bec531625a5c604767e3a7731f298
Bardia Shahrestani <Bardia.shahrestani@gmail.com>
2024-05-19 04:03:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function generateFilename() {
 
 function saveFile() {
     const text = document.getElementById('textArea').value;
-    const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+    const blob = new Blob([new TextEncoder().encode(text)], { type: 'text/plain;charset=utf-8' });
     const a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
     a.download = generateFilename();
