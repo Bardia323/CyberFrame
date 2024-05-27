efd9b83fbc93581246cb71f78bdbebe38fde3d1d
Bardia Shahrestani <Bardia.shahrestani@gmail.com>
2024-05-27 04:42:57 -0400

    added stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v2';  // Increment this value every time you update the cache contents
+const CACHE_NAME = 'v3';  // Increment this value every time you update the cache contents
 const urlsToCache = [
   '/',
   '/index.html',
